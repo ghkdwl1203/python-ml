4c1d094b3b73266bb3254e1652b4d06bf273799f
jyhwang2 <12@ssu.ac.kr>
2019-01-08 16:26:39 +0900

    2019-01-07

--- JumpToPython/종합문제/problem5.py
+++ JumpToPython/종합문제/problem5.py
@@ -5,7 +5,7 @@
 출력 예시: a3b2c6a1
 '''
 
-data = "aaabb11ccc"
+data = "ababb11ccc"
 
 count = 1
 sol = data[0]
@@ -13,11 +13,27 @@ for i in range(1, len(data)):
     if data[i-1] == data[i] :
         count +=1
     else :
-        sol = sol + str(count) + data[i+1]
+        sol = sol + str(count) + data[i]
         count=1
 sol = sol + str(count)
 
 print(sol)
 
-
-
+'''
+답
+def compress_string(s):
+    _c = ""
+    cnt = 0
+    result = ""
+    for c in s:
+        if c!=_c:
+            _c = c
+            if cnt: result += str(cnt)    ## if cnt : 의 의미.. cnt가 0이면 false, 그외 숫자면 true
+            result += c
+            cnt = 1
+        else:
+            cnt +=1
+    if cnt: result += str(cnt)
+    return result
+print (compress_string("aaabbcccccca")) #a3b2c6a1 출력
+'''
