4d0625c6ee7371266a338fd9a820c237b3836327
ghkdwl1203 <46411842+ghkdwl1203@users.noreply.github.com>
2019-01-13 22:38:42 +0900

    2019.01.06 Commit Test

--- JumpToPython/sample.py
+++ JumpToPython/sample.py
@@ -1,15 +1,4 @@
-def compress_string(s):
-    _c = ""
-    cnt = 0
-    result = ""
-    for c in s:
-        if c!=_c:
-            _c = c
-            if cnt: result += str(cnt)    ## if cnt : 의 의미.. cnt가 0이면 false, 그외 숫자면 true
-            result += c
-            cnt = 1
-        else:
-            cnt +=1
-    if cnt: result += str(cnt)
-    return result
-print (compress_string("aaabbcccccca")) #a3b2c6a1 출력
+python -m pip install pip
+
+
+
