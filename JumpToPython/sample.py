20a72a3b40816721bb1ef9bc1fc5b5b80b4c41b8
jyhwang2 <ghkdwl1203@ssu.ac.kr>
2019-01-17 20:51:46 +0900

    update

--- JumpToPython/sample.py
+++ JumpToPython/sample.py
@@ -1,4 +1,15 @@
-python -m pip install pip
-
-
-
+dic = {
+    '.-':'A','-...':'B','-.-.':'C','-..':'D','.':'E','..-.':'F',
+    '--.':'G','....':'H','..':'I','.---':'J','-.-':'K','.-..':'L',
+    '--':'M','-.':'N','---':'O','.--.':'P','--.-':'Q','.-.':'R',
+    '...':'S','-':'T','..-':'U','...-':'V','.--':'W','-..-':'X',
+    '-.--':'Y','--..':'Z'
+} ## 저자는 Dictionary 씀
+def morse(src):
+    result = []
+    for word in src.split("  "): ## 띄어쓰기 구분
+        for char in word.split(" "): ## 글자 구분
+            result.append(dic[char]) ## 글자 찾기
+        result.append(" ") ## 띄어쓰기인 경우 space 추가
+    return "".join(result)
+print(morse('.... .  ... .-.. . . .--. ...  . .- .-. .-.. -.--'))
