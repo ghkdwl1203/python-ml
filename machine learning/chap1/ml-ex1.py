524fcc24df5d2f9faec9031d3c230660b68edf57
ghkdwl1203 <46411842+ghkdwl1203@users.noreply.github.com>
2019-01-17 23:59:45 +0900

    2019.01.06 Commit Test

--- machine learning/chap1/ml-ex1.py
+++ machine learning/chap1/ml-ex1.py
@@ -6,4 +6,10 @@ savename = "test.png"
 urllib.request.urlretrieve(url,savename)
 print ("저장되었습니다.")
 
-$ python3 download-png1.py
+'$ python3 download-png1.py 명령줄?'
+
+mem = urllib.request.urlopen(url).read()
+
+with open(savename,mode="WB") as f:  ' WITH 자동으로 열고닫기처리'
+    f.write(mem)
+    print("저장되었습니다.")
