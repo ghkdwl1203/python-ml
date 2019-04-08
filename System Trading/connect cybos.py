0552ba5909b4c8809267e6719b5053209507f59b
ghkdwl1203 <46411842+ghkdwl1203@users.noreply.github.com>
2019-04-08 23:02:27 +0900

    update

--- System Trading/connect cybos.py
+++ System Trading/connect cybos.py
@@ -1,7 +1,7 @@
 from pandas import DataFrame
 import win32com.client
 import win32com
-
+from subDS import subStockChart # 위에 작성한 subStockChart 불러오기
 
 instCpStockCode = win32com.client.Dispatch("CpUtil.CpStockCode")
 print(instCpStockCode.GetData(1, 240810))   # 첫번째 인자 값이 0이면 종목코드, 1이면 좀목명, 2면 Fullcode 리턴
@@ -10,25 +10,26 @@ print(instCpStockCode.GetCount()) # 상장종목수
 
 
 stockNum = instCpStockCode.GetCount()
-
+list = []
 # 종목코드, 종목명 찾는 로직
 for i in range(stockNum):
     if instCpStockCode.GetData(1, i) == '원익IPS':
         print(" 종목코드 : ",instCpStockCode.GetData(0,i))
         print(" 종목명 : ",instCpStockCode.GetData(1,i))
         print(i)
-        temp = instCpStockCode.GetData(0,i)
+        list.append(instCpStockCode.GetData(0, i))
+        print(list)
+    if instCpStockCode.GetData(1, i) == '비에이치':
+        print(" 종목코드 : ",instCpStockCode.GetData(0,i))
+        print(" 종목명 : ",instCpStockCode.GetData(1,i))
+        print(i)
+        list.append(instCpStockCode.GetData(0,i))
+        print(list)
 
-
-# ###################################################################################################
-# ### file Name: practice_1_2_2_01.py
-# ###################################################################################################
-from pandas import DataFrame
-from subDS import subStockChart # 위에 작성한 subStockChart 불러오기
 
 if __name__ == "__main__":
     ### 자료가져오기
-    code =temp # 삼성전자 종목코드
-    numHist =100 # 과거 100일치 데이터
+    code =list # 삼성전자 종목코드
+    numHist =1 # 과거 100일치 데이터
     df=subStockChart(code, numHist)
     print(df)
